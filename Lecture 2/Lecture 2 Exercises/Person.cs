6e675eb31581645eb11002d177e8cb6e80a50242
Jacky Quan Cam Trinh <jtrinh18@student.aau.dk>
2020-08-17 11:04:47 +0200

    Opgave 1

--- Lecture 2/Lecture 2 Exercises/Person.cs
+++ Lecture 2/Lecture 2 Exercises/Person.cs
@@ -7,8 +7,92 @@ namespace Lecture_1
 {
     public class Person
     {
-        string FirstName;
-        string LastName;
-        int Age;
+        private string _Firstname;
+        private string _Lastname;
+        private int _Age;
+        private Person _Mother;
+        private Person _Father;
+
+        public string FirstName
+        {
+            //GetFirstName - læs firstname variabel 
+            get { 
+                return _Firstname;
+            }
+            //SetFisrtName - tilskriv firstname variabel værdi 
+            set 
+            {
+                if (value == null)
+                    return;
+                if (value.Length > 100)
+                    return;
+                for(int i = 0; i < value.Length; i++)
+                {
+                    if (!char.IsLetter(value[i]))
+                        return;
+                }
+                _Firstname = value;
+
+            }
+        }
+          
+        public string LastName
+        {
+            get { return _Lastname; }
+            set
+            {
+                if (value == null)
+                    return;
+                if (value.Length > 100)
+                    return;
+                for (int i = 0; i < value.Length; i++)
+                {
+                    if (!char.IsLetter(value[i]))
+                        return;
+                }
+                _Lastname = value;
+            }
+        }
+        public int Age 
+        {
+            get { return _Age; }
+            set 
+            {
+                if (value > 0)
+                    this._Age = value;
+            } 
+        }
+
+        public Person Mother
+        {
+
+            get
+            {
+                return _Mother;
+            }
+            set
+            {
+                if (value.Age <= this.Age)
+                    return;
+                _Mother = value;
+            }
+        }
+        public Person Father 
+        {
+            get
+            {
+                return _Father;
+            }
+            set
+            {
+                if (value.Age < this.Age)
+                    return;
+                _Father = value;
+            }
+            
+        }
+        
+
     }
+    
 }
--- Lecture 2/Lecture 2 Exercises/PersonGenerator.cs
+++ Lecture 2/Lecture 2 Exercises/PersonGenerator.cs
@@ -6,5 +6,49 @@ namespace Lecture_1
 {
     public class PersonGenerator
     {
+        public Person GeneratePerson()
+        {
+            Person Person = new Person();
+            Person.FirstName = "Adam";
+            Person.LastName = "Smith";
+            Person.Age = 36;
+
+            return Person;
+
+        }
+
+        public Person GenerateFamaly()
+        {
+            Person gustav = new Person();
+            gustav.FirstName = "Gustav";
+            gustav.LastName = "Rich";
+            gustav.Age = 66;
+
+            Person Person = new Person();
+            elisa.FirstName = "Elisa";
+            elisa.LastName = "Johnson";
+            elisa.Age = 65;
+
+            Person Person = new Person();
+            waren.FirstName = "Waren";
+            waren.LastName = "Rich";
+            waren.Age = 36;
+
+            Person Person = new Person();
+            anna.FirstName = "Anna";
+            anna.LastName = "Smith";
+            anna.Age = 38;
+
+            Person Person = new Person();
+            robin.FirstName = "Robin";
+            robin.LastName = "Rich";
+            robin.Age = 10;
+
+
+
+            return Person;
+        }
+
+
     }
 }
